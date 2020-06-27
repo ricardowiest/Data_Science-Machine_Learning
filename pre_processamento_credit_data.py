8698aa5d97e46a95abc6f68f5f822e1871617803
Ricardo Wiest <ricardowiest@gmail.com>
2020-06-27 14:14:17 -0300

    Create pre_processamento_credit_data.py

---
+++ pre_processamento_credit_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+base = pd.read_csv('credit_data.csv') #variavel le o arquivo
+base.describe()  
+base.loc[base['age'] < 0] #seleciona a base idade para verificar erro < 0
+
+# Preencher valores (idades) com erros pela média
+base.mean() #visualisa a média
+base['age'][base.age > 0].mean() #busca média da idade excluindo as idades < 0
+
+base.loc[base.age < 0, 'age'] = 40.92 #altera idades negativas para média
+
